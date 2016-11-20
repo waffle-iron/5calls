4ae0503af6854558714d07cf28077d261ec32d78
Nick O'Neill <nick.oneill@gmail.com>
2016-11-19 21:55:06 -0800

    Ugh, many-many.

--- app/models.py
+++ app/models.py
@@ -1,17 +1,18 @@
-from sqlalchemy import Column, Integer, String, Table, relationship, backref
+from sqlalchemy import Column, Integer, String, Text, Table, ForeignKey
+from sqlalchemy.orm import relationship
 from app.database import Base
 
-callees = Table('callees',
-    Column('issue_id', Integer, ForeignKey('issue.id')),
-    Column('callee_id', Integer, ForeignKey('callee.id'))
+issue_callees = Table('issue_callees', Base.metadata,
+	Column('issue_id', ForeignKey('issue.id'), primary_key=True),
+	Column('callee_id', ForeignKey('callee.id'), primary_key=True)
 )
 
 class Issue(Base):
 	__tablename__ = 'issues'
 	id = Column(Integer, primary_key=True)
 	name = Column(String(255), unique=False)
-	script = Column(String(1024), unique=False)
-	callees = relationship('Callee', secondary=callees, backref=backref('issues', lazy='dynamic'))
+	script = Column(Text)
+	callees = relationship('Callee', secondary=issue_callees, back_populates='issues')
 
 	def __init__(self, name):
 		self.name = name
@@ -23,6 +24,7 @@ class Callee(Base):
 	__tablename__ = 'callees'
 	id = Column(Integer, primary_key=True)
 	name = Column(String(255), unique=False)
+	issues = relationship('Issue', secondary=issue_callees, back_populates='callees')
 
 	def __init__(self, name):
 		self.name = name
