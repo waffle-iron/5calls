2671e070fc63599d29592fde1cf1760cda9d328c
Liam Campbell <liam@muledesign.com>
2016-11-21 22:59:21 -0800

    Useless although technically complete Choo app

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,32 @@
 const choo = require('choo');
+const html = require('choo/html');
 
-console.log(choo);
+const app = choo();
+
+app.model({
+  state: {
+    zip: '00501',
+    activeIssue: false,
+    issues: []
+  },
+  reducers: {
+
+  },
+  effects: {
+
+  }
+});
+
+const initialView = (state, prev, send) => {
+  return html`
+    <main role="main">
+      <p>(choo has taken over rendering)</p>
+    </main>
+  `;
+}
+
+app.router((route) => [
+  route('/', initialView)
+]);
+
+const tree = app.start('#root');
