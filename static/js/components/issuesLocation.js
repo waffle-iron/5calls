b23f4cc974b5deacc8df21abad0b7bee8c334e5d
James Home <jh@jameshome.com>
2017-01-27 01:50:12 -0800

    WIP: styling

--- static/js/components/issuesLocation.js
+++ static/js/components/issuesLocation.js
@@ -2,22 +2,22 @@ const html = require('choo/html');
 
 module.exports = (state, prev, send) => {
   return html`
-    <div class="issues__subtitle">
+    <div class="issues__location">
     ${pretext(state)}
     </div>
   `;
 
   function pretext(state) {
     if (state.askingLocation) {
-      return html`<p><form onsubmit=${submitAddress}>Enter an address or zip code: <input autofocus="true" name="address" /><button>Go</button></form></p>`
+      return html`<p><form onsubmit=${submitAddress}><input autofocus="true" name="address" placeholder="Enter an address or zip code" /> <button>Go</button></form></p>`
     } else {
       if (state.address != '') {
-        return html`<p>Included reps for ${state.address} • <a href="#" onclick=${unsetLocation}>Change</a></p>`
+        return html`<p>for <a href="#" onclick=${enterLocation}>${state.address}</a></p>`
       } else if (state.cachedCity != '') {
-        return html`<p>We've included reps for ${state.cachedCity} • <a href="#" onclick=${enterLocation}>Change</a> ${debugText(state.debug)}</p>`
+        return html`<p>for <a href="#" onclick=${enterLocation}> ${state.cachedCity}</a> ${debugText(state.debug)}</p>`
       } else {
-        return html`<p>Couldn't find your location. <a href="#" onclick=${enterLocation}>Change</a></p>`
-      }      
+        return html`<p><a href="#" onclick=${enterLocation}>Choose a location</a></p>`
+      }
     }
   }
 
