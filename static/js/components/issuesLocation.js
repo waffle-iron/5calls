2e6d1029569a6555298cacd2dd6a0096d73d2585
Liam Campbell <liam@muledesign.com>
2017-01-29 20:54:19 -0800

    Replace links with buttons on issuesLocation non-form states

--- static/js/components/issuesLocation.js
+++ static/js/components/issuesLocation.js
@@ -9,20 +9,26 @@ module.exports = (state, prev, send) => {
 
   function pretext(state) {
     if (state.askingLocation) {
-      return html`<p><form onsubmit=${submitAddress}><input type="text" autofocus="true" name="address" placeholder="Enter an address or zip code" /> <button>Go</button></form></p>`
+      return html`
+        <p>
+          <form onsubmit=${submitAddress}>
+            <input type="text" autofocus="true" name="address" placeholder="Enter an address or zip code" /> 
+            <button>Go</button>
+          </form>
+        </p>`
     } else {
       if (state.address != '') {
-        return html`<p>for <a href="#" onclick=${enterLocation}>${state.address}</a></p>`
+        return html`<p>for <button class="subtle-button" onclick=${enterLocation}>${state.address}</button></p>`
       } else if (state.cachedCity != '') {
-        return html`<p>for <a href="#" onclick=${enterLocation}> ${state.cachedCity}</a> ${debugText(state.debug)}</p>`
+        return html`<p>for <button class="subtle-button" onclick=${enterLocation}> ${state.cachedCity}</button> ${debugText(state.debug)}</p>`
       } else {
-        return html`<p><a href="#" onclick=${enterLocation}>Choose a location</a></p>`
+        return html`<p><button class="subtle-button" onclick=${enterLocation}>Choose a location</button></p>`
       }
     }
   }
 
   function debugText(debug) {
-    return debug ? html`<a href="#" onclick=${unsetLocation}>reset</a>` : html``;
+    return debug ? html`<button onclick=${unsetLocation}>reset</button>` : html``;
   }
 
   function submitAddress(e) {
