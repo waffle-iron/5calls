f342410019c0f8bb7faeb90b5a05f5a40926baac
Nick O'Neill <nick.oneill@gmail.com>
2017-01-31 21:22:06 -0800

    Revert "Have the browser request your location without asking"
    This reverts commit 2ba11b3d9db6f1e0af192ddb41c33c58545065e0.

--- static/js/components/issuesLocation.js
+++ static/js/components/issuesLocation.js
@@ -1,9 +1,6 @@
 const html = require('choo/html');
 
 module.exports = (state, prev, send) => {
-  if (state.askingLocation && !state.fetchingLocation && state.geolocation === '') {
-    fetchBrowserLocation(null);
-  }
   return html`
     <div class="issues__location">
     ${pretext(state)}
@@ -15,7 +12,13 @@ module.exports = (state, prev, send) => {
       return html`<p class="loadingAnimation">Getting your location</p>`;
     }
     else if (state.askingLocation) {
-      return html`<p><form onsubmit=${submitAddress}><input type="text" autofocus="true" name="address" placeholder="Enter an address or zip code" /> <button>Go</button></form></p>`;
+      if (state.locationFetchType === 'browserGeolocation') {
+        return html`<p>
+          <button onclick=${fetchBrowserLocation}>Geolocate</button> or <button onclick=${fetchLocationBy} data-by='addressForm'>Enter Address</button>
+        </p>`
+      } else {
+        return html`<p><form onsubmit=${submitAddress}><input type="text" autofocus="true" name="address" placeholder="Enter an address or zip code" /> <button>Go</button></form></p>`;
+      }
     } else {
       if (state.address != '') {
         return html`<p>for <a href="#" onclick=${enterLocation}>${state.address}</a></p>`
@@ -27,6 +30,56 @@ module.exports = (state, prev, send) => {
         return html`<p><a href="#" onclick=${enterLocation}>Couldn't find your location</a></p>`
       }
     }
+  }
+
+  function fetchLocationBy(e) {
+    send('fetchLocationBy', e.target.dataset.by);
+  }
+
+  // TODO: Set geolocation cache time in case we want to bring it back
+  // TODO: Handle when a user blocks browser geolocation, but tries to reenable in a new session
+  function fetchBrowserLocation(e) {
+    let pos;
+    let nudgeTimeoutId = setTimeout(showNudgeAlert, 5000);
+
+    let showNudgeAlert = function() {
+      window.alert('Too slow');
+    }
+
+    let geoSuccess = function(position) {
+      clearTimeout(nudgeTimeoutId);
+      send('fetchingLocation', false);
+
+      if (typeof position.coords !== 'undefined') {
+        let lat = position.coords.latitude;
+        let long = position.coords.longitude;
+
+        if (lat && long) {
+          let geo = Math.floor(lat*10000)/10000 + ',' + Math.floor(long*10000)/10000;
+          send('allowBrowserGeolocation', true);
+          send('setBroswerGeolocation', geo);
+        } else {
+          console.log("Error: bad browser location results");
+          send('fetchLocationBy', null);
+        }
+      } else {
+        console.log("Error: bad browser location results");
+        send('fetchLocationBy', null);
+      }
+    }
+
+    let geoError = function(error) {
+      send('fetchingLocation', false);
+
+      if (error.code === 1) {
+        send('allowBrowserGeolocation', false);
+      }
+      send('fetchLocationBy', null);
+      console.log("Error with browser location (code: " + error.code + ")");
+    }
+
+    navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+    send('fetchingLocation', true);
   }
 
   function fetchLocationBy(e) {
