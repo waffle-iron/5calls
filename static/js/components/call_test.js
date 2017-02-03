b3703494c44383d75eed2801b18fa5beaa6805ce
Craig Doremus <cdoremus@gmail.com>
2017-02-03 15:55:07 -0500

    Added  tests; cleaned up others

--- static/js/components/call_test.js
+++ static/js/components/call_test.js
@@ -14,19 +14,35 @@ describe('call component', () => {
     expect(result.toString()).to.contain(expectSubstr);
   });
 
-  it('should return contact information', () => {
-    // For simplicity, we're setting up one issue and contact
-    let expectSubstr = 'Senator Blowhart';
-    let id = 1;
-    let location = {params: {issueid: id}};
-    let issue = {id: id, name: 'Bozo the nominee', reason: 'crazy', script: 'Please vote against everything'};
-    let contact = {name: expectSubstr, party: 'Dem'};
-    issue.contacts = [contact];
-    let issues = [issue];
-    let state = {issues, location, contactIndex: 0};
-    let result = call(state);
-    let el = result.toString();
-    // console.log('html: ', el);
-    expect(el).to.contain(expectSubstr);
+  describe('contact section', () => {
+
+    it('should display contact if contact data is present in state', () => {
+      let cname = 'Senator Blowhart';
+      let id = 1;
+      let location = {params: {issueid: id}};
+      let issue = {id: id, name: 'Bozo the nominee', reason: 'crazy', script: 'Please vote against everything'};
+      let contact = {name: cname, party: 'Dem'};
+      issue.contacts = [contact];
+      let issues = [issue];
+      let state = {issues, location, contactIndex: 0};
+      let result = call(state);
+      let h3s = result.getElementsByTagName('h3');
+      // First h3 should contain 'Call this office'
+      expect(h3s[0].childNodes[0].data).to.contain('Call this office');
+    });
+
+    it('should display "no contact" message if no contact data is NOT present in state', () => {
+      let id = 1;
+      let location = {params: {issueid: id}};
+      let issue = {id: id, name: 'Bozo the nominee', reason: 'crazy', script: 'Please vote against everything'};
+      issue.contacts = [null];
+      let issues = [issue];
+      let state = {issues, location, contactIndex: 0};
+      let result = call(state);
+      let as = result.getElementsByTagName('a');
+      // console.log('as children data', as[0].childNodes[0].data);
+      // First h2 should contain 'to find your representatives'
+      expect(as[0].childNodes[0].data).to.contain('Set your location');
+    });
   });
 });
