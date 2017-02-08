4b43398d403ae7d05262c6945b025c46ddacc9a9
Nick O'Neill <nick.oneill@gmail.com>
2017-02-08 08:40:57 -0800

    Add more clarity to local numbers.

--- static/js/components/contact.js
+++ static/js/components/contact.js
@@ -13,7 +13,7 @@ module.exports = (c, state, prev, send) => {
   let fieldOffices
   if (c.field_offices) {
     fieldOffices = html`
-      <p class="call__contact__show-field-offices">Busy line? <a onclick=${() => {send('toggleFieldOfficeNumbers')}}>Call a local office</a></p>
+      <p class="call__contact__show-field-offices">Busy line? <a onclick=${() => {send('toggleFieldOfficeNumbers')}}>Click here to see local office numbers</a></p>
     `
     if (state.showFieldOfficeNumbers) {
       fieldOffices = html`
