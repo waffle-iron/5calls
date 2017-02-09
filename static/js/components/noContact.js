72142c79ba6fc580fc4ef357aa7bc786273447df
Coleman Collins <coleman@colemancollins.com>
2017-02-09 15:46:15 -0600

    different no contacts message if no contacts AND split district; partial fix to #64

--- static/js/components/noContact.js
+++ static/js/components/noContact.js
@@ -7,7 +7,21 @@ module.exports = (state, prev, send) => {
     send('enterLocation');
   }
 
-	return html`<div class="call__nocontact">
-		<h2><a onclick=${(e) => enterLocation(e)}>Set your location</a> to find your representatives</h2>
-	</div>`
+  function noContactsMessage(state) {
+    if (state.splitDistrict == true && (state.address || state.cachedCity)) {
+    // if it's a split district but we have a location set
+      return html`<div>
+                    <p>The location you provided could be in one of two congressional districts.</p>
+                    <p><a onclick=${(e) => enterLocation(e)}>Enter your full address or Zip+4</a> to identify your representative in the House.</p>
+                  </div>`
+    }
+    else {
+      return html`<h2><a onclick=${(e) => enterLocation(e)}>Set your location</a> to find your representatives</h2>`
+    }
+  }
+
+	return html`
+    <div class="call__nocontact">
+		  ${noContactsMessage(state)}
+	  </div>`
 }
