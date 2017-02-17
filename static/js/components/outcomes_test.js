18cd4e08c24b5fbf3338213aa12c16eaa56102ab
bernardwang <bernard@bernard.wang>
2017-02-17 00:06:06 -0600

    Add new test cases

--- static/js/components/outcomes_test.js
+++ static/js/components/outcomes_test.js
@@ -53,4 +53,56 @@ describe('outcomes component', () => {
       expect(result).to.be.undefined;
   });
 
+  it('should have contacts left if last contact', () => {
+      let cname1 = 'Senator Blowhart';
+      let cname2 = 'Senator Hartblow';
+      let id = 1;
+      let location = {params: {issueid: id}};
+      let issue = {
+        id: id, // does not match location issue id
+        name: 'Bozo the nominee',
+        reason: 'crazy',
+        script: 'Please vote against nominee Bozo'
+      };
+      let contact1 = {name: cname1, party: 'Dem'};
+      let contact2 = {name: cname2, party: 'Dem'};
+      issue.contacts = [contact1, contact2];
+      let issues = [issue];
+      let contactIndices = {};
+      contactIndices[id] = 0;
+      let state = {
+        issues,
+        location,
+        contactIndices: contactIndices,
+      };
+      let result = outcomes(state);
+      expect(result.textContent).to.contain('1 more');
+  });
+
+  it('should NOT have contacts left if last contact', () => {
+      let cname1 = 'Senator Blowhart';
+      let cname2 = 'Senator Hartblow';
+      let id = 1;
+      let location = {params: {issueid: id}};
+      let issue = {
+        id: id, // does not match location issue id
+        name: 'Bozo the nominee',
+        reason: 'crazy',
+        script: 'Please vote against nominee Bozo'
+      };
+      let contact1 = {name: cname1, party: 'Dem'};
+      let contact2 = {name: cname2, party: 'Dem'};
+      issue.contacts = [contact1, contact2];
+      let issues = [issue];
+      let contactIndices = {};
+      contactIndices[id] = 1;
+      let state = {
+        issues,
+        location,
+        contactIndices: contactIndices,
+      };
+      let result = outcomes(state);
+      expect(result.textContent).to.not.contain('1 more');
+  });
+
 });
