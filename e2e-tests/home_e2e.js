bd869d9095a1b6221ea964c0afa022bef843bf12
Craig Doremus <cdoremus@gmail.com>
2017-02-17 23:35:21 -0500

    Added docs. Moved driver.get() to beforeEach block

--- e2e-tests/home_e2e.js
+++ e2e-tests/home_e2e.js
@@ -1,18 +1,26 @@
+/**
+ * End-to-end tests for home page
+ *
+ * To run tests:
+ * 1. In a command window:
+ *    cd go
+ *    make run
+ * 2. In a second command window:
+ *    gulp
+ * 3. In a third command window:
+ *    npm run test:e2e
+ */
 const webdriver = require('selenium-webdriver');
+const test = require('selenium-webdriver/testing');
 const chai = require('chai');
 const expect = chai.expect;
 const By = webdriver.By;
 const until = webdriver.until;
 
-var chaiAsPromised = require('chai-as-promised');
-chai.use(chaiAsPromised);
-
-var test = require('selenium-webdriver/testing');
-
-const mochaTimeOut = 30000; //ms
-
 const url = 'http://localhost:8000/';
+
 let driver;
+
 test.before(() => {
   driver = new webdriver.Builder()
     .withCapabilities({
@@ -24,60 +32,25 @@ test.before(() => {
             }
           }
       }).build();
-//   // console.log('TIMEOUT', timeout);
-//   // timeout = 10000;
-//   driver = new webdriver.Builder()
-//     .forBrowser('chrome')
-//     .build();
 });
 
 test.after(() => {
-  // if (driver) {
+  if (driver) {
     driver.quit();
-  // }
-  // done();
+  }
 });
 
 test.describe('home page', () => {
-  // this.timeout(mochaTimeOut);
-  let expected = '5 Calls: Make your voice heard';
-  // test.beforeEach(() => {
-  //   // driver.get(url).then((resolve) => resolve());
-  //     // .then(() => {
-  //     // console.log('URL opened: ' + url);
-  //     // done();
-  //   });
-  // });
 
-  // afterEach(() => done());
-
-  test.it('should have correct page title', (done) => {
+  test.beforeEach(() =>{
     driver.get(url);
+  });
+
+  test.it('should show correct page title', (done) => {
+    let expected = '5 Calls: Make your voice heard';
     return driver.getTitle().then((title)=> {
-      console.log('TITLE', title);
       expect(title).to.equal(expected);
       done();
     });
-    // return expect(driver.getTitle()).to.eventually.equal('foobar');
-    // let about = driver.findElement(By.css('a[href="#privacy"'));
-
-    // driver.findElement(By.css('a[href="https://5calls.org/privacy.html"')).click()
-      // .then(() => done());
-      // .then((result) => {
-      //   console.log('about link', result);
-      //   done();
-      // },
-      //   () => {
-      //     done ()
-      // });
-    //  about.click();
-    //  done();
-    // let title = driver.getTitle();
-    // return title.then((title) => {
-    //   console.log('TITLE', title);
-    //   expect(title).to.equal(expected);
-    //   // expect(title).to.equal('foobar');
-    // })
-    // .then(() => title.resolve());
   });
 });
