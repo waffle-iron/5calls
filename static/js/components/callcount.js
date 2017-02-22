dc5a59ef5ff861d532a45968763eb3ae551d3522
Owen Derby <ocderby@gmail.com>
2017-02-21 19:08:19 -0800

    Fix #197 - add support for rendering call count on Safari
    Given that we have a restricted subset of the toLocaleString
    functionality we need (just inserting thousands separator) and
    we only support a single locale now ('en') and that we are a
    US-centric app, I decided to just special case handling in
    the component for now.

--- static/js/components/callcount.js
+++ static/js/components/callcount.js
@@ -3,7 +3,19 @@ const html = require('choo/html');
 module.exports = (state, prev, send) => {
   return html`
   <h2 class="callcount" onload=${(e) => send('getTotals')}>
-    Together we’ve made ${Number(state.totalCalls).toLocaleString()} calls
+    Together we’ve made ${callCount(state)} calls
   </h2>
   `;
+
+  function callCount(state) {
+    const calls = Number(state.totalCalls);
+    // Number.toLocaleString() doesn't work on Safari 9 (see https://github.com/5calls/5calls/issues/197)
+    if (!!window.Intl && typeof Intl.NumberFormat == 'function') {
+      return calls.toLocaleString();
+    } else {
+      // As a fallback, use a quick-and-dirty regex to insert commas.
+      // When in doubt, get code from stackoverflow: http://stackoverflow.com/a/2901298/7542666
+      return calls.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+  }
 }
