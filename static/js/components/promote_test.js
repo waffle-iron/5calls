ef60500e614db14f24edaa3967e78b32c0610f5f
Nick O'Neill <nick.oneill@gmail.com>
2017-03-05 22:44:49 -0800

    I think this is the right way to fix those tests.

--- static/js/components/promote_test.js
+++ static/js/components/promote_test.js
@@ -6,16 +6,16 @@ const expect = chai.expect;
 describe('promote component', () => {
   it('should display generic tweet link if issue does not exist', () => {
     let issue = undefined;
-    let state = {};
-    let result = promote(state, null, null, issue);
+    let state = {location: {params: {issueid: null}}};
+    let result = promote(state, null, null);
     let element = result.querySelector('a');
     expect(element.textContent).to.contain('Share on Twitter');
   });
 
   it('should display tweet link if issue is defined', () => {
     let issue = {id: 1, name: 'Save ACA'};
-    let state = {};
-    let result = promote(state, null, null, issue);
+    let state = {issues: [issue], location: {params: {issueid: 1}}};
+    let result = promote(state, null, null);
     let element = result.querySelector('a');
     expect(element.textContent).to.contain('Tweet this issue');
   });
