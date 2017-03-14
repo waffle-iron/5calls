e679bb059031f35e861dacd87f1578f3987a066a
Jeremy Schrader <jschrader@wintellect.com>
2017-03-14 14:18:45 -0700

    usediv in translation tests

--- static/js/utils/translation_test.js
+++ static/js/utils/translation_test.js
@@ -55,63 +55,87 @@ describe('translation', () => {
       let result = t.getText(key, null, true);
       expect(result).to.equal(expected);
     });
+
+    it('should return the phrase in a "span" tag when fourth, "useDiv", argument is false', () => {
+      let key = 'common.reset';
+      let expected = '<span>reset</span>';
+      let result = t.getText(key, null, false, false);
+      expect(result.outerHTML).to.equal(expected);      
+    });
+
+    it('should return the phrase in no tag when third argument(justText) is true and fourth(useDiv) argument is true', () => {
+      let key = 'common.reset';
+      let expected = 'reset';
+      let result = t.getText(key, null, true, true);
+      expect(result).to.equal(expected);
+    });
+
+    it('should return the phrase in a "div" tag when fourth, "useDiv", argument is true', () => {
+      let key = 'common.reset';
+      let expected = '<div>reset</div>';
+      let result = t.getText(key, null, false, true);
+      expect(result.outerHTML).to.equal(expected);      
+    });
   });
 
-  it('should return the english localized string when given the key', () => {
-    let key = 'common.reset';
-    let expected = 'reset';
-    let result = t.getText(key, null, true);
-    expect(result).to.equal(expected);
+  describe('simple localization', () => {
+    it('should return the english localized string when given the key', () => {
+      let key = 'common.reset';
+      let expected = 'reset';
+      let result = t.getText(key, null, true);
+      expect(result).to.equal(expected);
+    });
   });
 
-  it('should return the "1000 Calls" when given 1000 showing that we have not implement number formatting', () => {
-    let key = 'common.callWithCount';
-    let options = {'count': 1000};
-    let expected = '1000 calls';
-    let result = t.getText(key, options, true);
-    expect(result).to.equal(expected);
-  });
+    describe('pluralization and interpolation', () => {
+      it('should return the "1000 Calls" when given 1000 showing that we have not implement number formatting', () => {
+        let key = 'common.callWithCount';
+        let options = {'count': 1000};
+        let expected = '1000 calls';
+        let result = t.getText(key, options, true);
+        expect(result).to.equal(expected);
+      });
 
-  it('should return the singular version of person based on the option provided', () => {
-    let key = 'common.person';
-    let options = {count: 1};
-    let expected = 'person';
-    let result = t.getText(key, options, true);
-    expect(result).to.equal(expected);
-  });
+      it('should return the singular version of person based on the option provided', () => {
+        let key = 'common.person';
+        let options = {count: 1};
+        let expected = 'person';
+        let result = t.getText(key, options, true);
+        expect(result).to.equal(expected);
+      });
 
-  it('should return the plural version of person based on the option provided', () => {
-    let key = 'common.person';
-    let options = {count: 2};
-    let expected = 'people';
-    let result = t.getText(key, options, true);
-    expect(result).to.equal(expected);
-  });
+      it('should return the plural version of person based on the option provided', () => {
+        let key = 'common.person';
+        let options = {count: 2};
+        let expected = 'people';
+        let result = t.getText(key, options, true);
+        expect(result).to.equal(expected);
+      });
 
+      it('should return the singular version of person based on the option provided', () => {
+        let key = 'outcomes.contactsLeft';
+        let options = {contactsRemaining: 1};
+        let expected = 'person';
+        let result = t.getText(key, options, true);
+        expect(result).to.contain(expected);
+      });
 
-  it(', interpolating and then pluralizing, should return the singular version of person based on the option provided', () => {
-    let key = 'outcomes.contactsLeft';
-    let options = {contactsRemaining: 1};
-    let expected = 'person';
-    let result = t.getText(key, options, true);
-    expect(result).to.contain(expected);
-  });
+      it('should return the pluralized version of person based on the option provided', () => {
+        let key = 'outcomes.contactsLeft';
+        let options = {contactsRemaining: 2};
+        let expected = 'people';
+        let result = t.getText(key, options, true);
+        expect(result).to.contain(expected);
+      });
 
-  it(', interpolating and then pluralizing, should return the pluralized version of person based on the option provided', () => {
-    let key = 'outcomes.contactsLeft';
-    let options = {contactsRemaining: 2};
-    let expected = 'people';
-    let result = t.getText(key, options, true);
-    expect(result).to.contain(expected);
-  });
-
-  it(', interpolating and then pluralizing, should return the pluralized version of person for "0" people', () => {
-    let key = 'outcomes.contactsLeft';
-    let options = {'contactsRemaining': 0};
-    let expected = 'people';
-    let result = t.getText(key, options, true);
-    expect(result).to.contain(expected);
-  });
+      it('should return the pluralized version of person for "0" people', () => {
+        let key = 'outcomes.contactsLeft';
+        let options = {'contactsRemaining': 0};
+        let expected = 'people';
+        let result = t.getText(key, options, true);
+        expect(result).to.contain(expected);
+      });
+    });
 });
 
 
