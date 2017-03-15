ca31a0555643e42790fc1d24ef533318343f863c
Jeremy Schrader <jschrader@wintellect.com>
2017-03-14 21:42:37 -0700

    Fix noContact and the issue of passing a function into a localized string.

--- static/js/components/noContact.js
+++ static/js/components/noContact.js
@@ -4,6 +4,16 @@ const t = require('../utils/translation');
 const find = require('lodash/find');
 
 module.exports = (state, prev, send) => {
+  function initializeFragment(e) {
+    keys = ['lnkEnterAddress', 'lnkSetYourLocation'];
+    keys.map((k) => {
+      let el = document.getElementById(`${k}`);
+      if (el) {
+        el.addEventListener("click", (e)=>enterLocation(e), false);
+      }
+    });
+  }
+
   function enterLocation(e) {
     e.preventDefault();
     send('enterLocation');
@@ -11,17 +21,17 @@ module.exports = (state, prev, send) => {
 
   function noContactsMessage(state) {
     if (state.splitDistrict && (state.address || state.cachedCity)) {
-      return html`<div>
+      return html`<div onload=${(e) => initializeFragment(e)}>
                     <p>${t.getText("noContact.oneOfTwoDistricts")}</p>
                     <p>${t.getText("noContact.enterYourLocation")}</p>
                   </div>`
     }
     else {
-      return html`<h2>${t.getText("noContact.setYourLocation")}</h2>`
+      return html`<h2 onload=${(e) => initializeFragment(e)}>${t.getText("noContact.setYourLocation")}</h2>`
     }
   }
 
-	return html`
+  return html`
     <div class="call__nocontact">
 		  ${noContactsMessage(state)}
 	  </div>`
