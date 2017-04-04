ead5e9470a2c2cc9c5270e183d1c823a6d502e28
Jeremy Schrader <jschrader@wintellect.com>
2017-04-04 09:20:21 -0700

    fix impact result

--- static/js/components/impactResult.js
+++ static/js/components/impactResult.js
@@ -5,11 +5,9 @@ module.exports = (state) => {
   const contactedCalls = state.userStats.contacted;
   const vmCalls = state.userStats.vm;
   const unavailableCalls = state.userStats.unavailable;
-  // Jeremy- TODO, when bernard has finalized his styling, need to handle clash with styling of span because I'm
-  // wrapping the localized text also in a span.
   return html`
-    <p class="impact_result">
-     ${t('impact.callSummaryText', {contactedCalls: contactedCalls, vmCalls: vmCalls, unavailableCalls: unavailableCalls})}
-    </p>
+    <div class="impact_result">
+     ${t('impact.callSummaryText', {contactedCalls: contactedCalls, vmCalls: vmCalls, unavailableCalls: unavailableCalls}, false, true)}
+    </div>
   `;
 }
--- static/js/components/outcomes.js
+++ static/js/components/outcomes.js
@@ -27,10 +27,10 @@ module.exports = (state, prev, send) => {
     return html`<div class="call__outcomes">
       <h3 class="call__outcomes__header">${t("outcomes.enterYourCallResult")}</h3>
       <div class="call__outcomes__items">
-        <button onclick=${() => outcome('unavailable')}>${t("outcomes.unavailable")}</button>
-        <button onclick=${() => outcome('vm')}>${t("outcomes.voicemail")}</button>
-        <button onclick=${() => outcome('contacted')}>${t("outcomes.madeContact")}</button>
-        <button onclick=${() => outcome()}>${t("outcomes.skip")}</button>
+        <button onclick=${(e) => outcome(e, 'unavailable')}>${t("outcomes.unavailable")}</button>
+        <button onclick=${(e) => outcome(e,'vm')}>${t("outcomes.voicemail")}</button>
+        <button onclick=${(e) => outcome(e,'contacted')}>${t("outcomes.madeContact")}</button>
+        <button onclick=${(e) => outcome(e)}>${t("outcomes.skip")}</button>
       </div>
 
       ${contactsLeft > 0 ? html`<h3 aria-live="polite" class="call__contacts__left" >${contactsLeftText}</h3>` : null}
