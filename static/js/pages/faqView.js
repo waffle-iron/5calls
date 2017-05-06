08db208cc21071f540e78f3546758ac49160907d
Nick O'Neill <nick.oneill@gmail.com>
2017-05-06 12:16:49 -0700

    faq view should have a footer.

--- static/js/pages/faqView.js
+++ static/js/pages/faqView.js
@@ -2,12 +2,16 @@ const html = require('choo/html');
 
 const sidebar = require('../components/sidebar.js');
 const faq = require('../components/faq.js');
+const footer = require('../components/footer.js');
 
 module.exports = (state, prev, send) => {
   return html`
-    <div id="root" class="layout">
-      ${sidebar(state, prev, send)} 
-      ${faq(state, prev, send)}
+    <div id="root">
+      <div class="layout">
+        ${sidebar(state, prev, send)} 
+        ${faq(state, prev, send)}
+      </div>
+      ${footer(state, prev, send)}
     </div>
   `;
 };
