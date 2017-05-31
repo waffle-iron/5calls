e80a798f709a0ceed873467097c33bfc4f6c7403
Greg Graf <me@greggraf.com>
2017-05-31 11:32:49 +0200

    rework test to use DOM API instead of string matching

--- static/js/components/footer_test.js
+++ static/js/components/footer_test.js
@@ -28,18 +28,28 @@ describe('footer component', () => {
   describe('language selection dropdown', () => {
   
     it('should display unselected language as link with globe icon', () => {
-
       let result = footer({
         selectedLanguage: 'es'
       });
 
-      expect(result.innerHTML).not.to.match(/select/);
-      expect(result.innerHTML).to.match(/<a href="#en">\s*<i aria-hidden="true" class="fa fa-globe"><\/i><span>English<\/span>\s*<\/a>/);
-      expect(result.innerHTML).not.to.match(/<b>Español<\/b>/);
+      var españolLink = result.querySelector('a[href="#es"]')
+      var englishLink = result.querySelector('a[href="#en"]')
+      var icon = englishLink.querySelector('i')
+      var label = englishLink.querySelector('span')
+      
+
+      expect(españolLink).not.to.exist;
+      expect(englishLink).to.exist;
+
+      expect(icon).to.exist;
+      expect(icon.getAttribute('aria-hidden')).to.equal('true');
+      expect(icon.getAttribute('class')).to.equal('fa fa-globe');
+      
+      expect(label).to.exist;
+      expect(label.innerHTML).to.equal("English");
     });
 
     it('should call the chooseLanguage effect when an link is clicked', () => {
-
       function sendSpy(effect, data) {
         sendSpy.effect = effect;
         sendSpy.data = data;
@@ -58,7 +68,6 @@ describe('footer component', () => {
 
       expect(sendSpy.effect).to.equal('chooseLanguage');
       expect(sendSpy.data).to.equal('en');
-
     });
   });
 
